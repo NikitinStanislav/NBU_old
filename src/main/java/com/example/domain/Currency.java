3e6619bdf78343048b7df4cb22a51746ddaefc51
NikitinStanislav <nikitinsa13@gmail.com>
2021-12-02 11:46:48 +0200

    tables relation setted, scheduler added

--- src/main/java/com/example/domain/Currency.java
+++ src/main/java/com/example/domain/Currency.java
@@ -1,17 +1,14 @@
 package com.example.domain;
 
 
-import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.NonNull;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
-import java.time.Instant;
+import java.util.Set;
 
 @Data //shortcut of @ToString, @EqualsAndHashCode, @Getter, @Setter and @RequiredArgsConstructor together
 @Entity
-@Table(name ="currencies")
 public class Currency {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,26 +17,20 @@ public class Currency {
     public Currency() {
     }
 
-    public Currency(@NonNull String abbreviation, @NonNull double rate, @NonNull int code, @NonNull String name, Instant date) {
+    public Currency(@NonNull String abbreviation,@NonNull int code, @NonNull String name) {
         this.abbreviation = abbreviation;
-        this.rate = rate;
         this.code = code;
         this.name = name;
-        this.date = date;
     }
 
     @NonNull
-    //@Column(unique = true)
+    @Column(unique = true)
     private String abbreviation;
 
-    @NonNull
-    private double rate;
     @NonNull
     private int code;
     @NonNull
     private String name;
-    @NotNull
-    private Instant date;
 
 
 }
